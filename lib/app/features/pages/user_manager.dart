1b0d4ecc7b31268afe201cfffa80148aa3c2a148
Oonailime <emiliano.guimaraes@ufba.br>
2024-06-25 01:06:29 -0300

    Boas práticas

--- lib/app/features/pages/user_manager.dart
+++ lib/app/features/pages/user_manager.dart
@@ -1,36 +1,6 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-
-
-class Usuario {
-  String username;
-  String password;
-  List<String> listasDeCompras; // Exemplo de listas de compras do usuário
-
-  Usuario({
-    required this.username,
-    required this.password,
-    this.listasDeCompras = const [], // Inicializa como uma lista vazia
-  });
-
-  // Método para converter usuário para JSON
-  Map<String, dynamic> toJson() {
-    return {
-      'username': username,
-      'password': password,
-      'listasDeCompras': listasDeCompras,
-    };
-  }
-
-  // Método para criar usuário a partir de JSON
-  factory Usuario.fromJson(Map<String, dynamic> json) {
-    return Usuario(
-      username: json['username'],
-      password: json['password'],
-      listasDeCompras: List<String>.from(json['listasDeCompras'] ?? []),
-    );
-  }
-}
+import 'package:app_lista_de_compras/app/features/model/usuario.dart';
 
 class UserManager {
   static const String _userKey = 'users';
