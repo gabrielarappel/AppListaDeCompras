8f37930083d1a4d4ed074b5615f7ecdfd34cd0be
Oonailime <emiliano.guimaraes@ufba.br>
2024-06-26 13:21:08 -0300

    O sistema web funciona perfeitamente com o firebase

--- lib/app/features/model/listadecompra.dart
+++ lib/app/features/model/listadecompra.dart
@@ -1,19 +1,26 @@
+import 'package:app_lista_de_compras/app/features/model/produto.dart';
 class ListaDeCompra {
   String id;
   String nome;
   double preco;
+  List<Produto> produtos;
 
   ListaDeCompra({
     required this.id,
     required this.nome,
     required this.preco,
+    required this.produtos,
   });
 
   factory ListaDeCompra.fromJson(Map<String, dynamic> json) {
+    var produtosList = json['produtos'] as List<dynamic>;
+    List<Produto> produtos = produtosList.map((item) => Produto.fromJson(item as Map<String, dynamic>)).toList();
+
     return ListaDeCompra(
       id: json['id'] as String,
       nome: json['nome'] as String,
       preco: (json['preco'] as num).toDouble(),
+      produtos: produtos,
     );
   }
 
@@ -22,6 +29,7 @@ class ListaDeCompra {
       'id': id,
       'nome': nome,
       'preco': preco,
+      'produtos': produtos.map((produto) => produto.toJson()).toList(),
     };
   }
 }
