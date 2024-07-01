28bc0aaf3e624556e37ef2f390f2ddabeebeab19
Oonailime <emiliano.guimaraes@ufba.br>
2024-07-01 13:31:55 -0300

    Tirei um import desnecessário

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:app_lista_de_compras/app/features/pages/splash.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
