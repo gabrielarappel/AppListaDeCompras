8f4e92b1a563e12fef617c03a9244b8be8729967
Oonailime <emiliano.guimaraes@ufba.br>
2024-07-03 00:12:11 -0300

    preparando a função de adicionar colaboradores

---
+++ lib/app/features/manager/notification_service.dart
@@ -0,0 +1,25 @@
+class NotificationService {
+  static int unreadNotificationsCount = 0;
+  static List<Map<String, dynamic>> notifications = [];
+
+  static void incrementNotificationCount() {
+    unreadNotificationsCount++;
+  }
+
+  static void clearNotifications() {
+    unreadNotificationsCount = 0;
+  }
+
+  static void addNotification(String title, String message) {
+    notifications.add({
+      'title': title,
+      'message': message,
+    });
+    incrementNotificationCount();
+  }
+
+  static void clearAllNotifications() {
+    notifications.clear();
+    clearNotifications();
+  }
+}
